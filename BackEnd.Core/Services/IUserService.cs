48a9717080087d201cd18f28e065f7a21c155c86
Amiran97 <amiran.todua@tangramltd.com>
2022-11-29 21:13:42 +0200

    Init project

---
+++ BackEnd.Core/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using BackEnd.Core.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.Core.Services
+{
+    public interface IUserService
+    {
+        User CurrentUset { get; }
+    }
+}
